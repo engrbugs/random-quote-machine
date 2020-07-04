d83b0250900071da1c6bd2d01c428004400602ad
engrbugs <engrbugs@gmail.com>
2020-07-03 20:47:27 -0700

    Updates

---
+++ precache-manifest.e0f6506493e240a44df4ca57082ebdcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f76ec491fd846e5eb4aba24cb981242",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "06e6317c1a142523c3f4",
+    "url": "/random-quote-machine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a403a08a55d032381c42",
+    "url": "/random-quote-machine/static/js/2.fb541593.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.fb541593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e6317c1a142523c3f4",
+    "url": "/random-quote-machine/static/js/main.ab1c6714.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
