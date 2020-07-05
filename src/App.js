b08f0ee756038ca27b3bb5fa398d957cec8843d7
engrbugs <engrbugs@gmail.com>
2020-07-04 18:24:28 -0700

    2020-07-04-1824

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import logo from './wheel.svg';
 import './App.css';
 
 const QUOTES = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 const tweetURL = 'https://twitter.com/intent/tweet?hashtags=quotes&hashtags=freecodecamp&text='
+const COLOR = [ '#ee5253', '#ff9f43', '#c8d6e5', '#576574', '#ff6b6b', '#8395a7', '#feca57', 
+                '#48dbfb', '#10ac84', '#01a3a4', '#2e86de', '#54a0ff', '#7f8c8d', '#e67e22',
+                '#2ecc71', '#16a085', '#2c3e50', '#f1c40f', '#8e44ad', '#6c5ce7', '#2d3436',
+                '#0984e3', '#fab1a0', '#ffeaa7', '#00b894', '#e84393', '#40739e', '#c23616']
 
 class App extends Component {
   state = {
     quotes: [],
-    index: 0
+    index: 0,
+    color: "#282c34"
   }
 
   componentDidMount() {
@@ -23,39 +29,43 @@ class App extends Component {
   RandomizeIndex = () => {
     const { quotes } = this.state;
     if (quotes.length > 0) {
+      let ci = Math.floor(Math.random() * COLOR.length);
       let index = Math.floor(Math.random() * quotes.length);
       this.setState({
-        index: index
+        index: index,
+        color: COLOR[ci]
       });
-      console.log(index);
+      console.log('index:', index);
+      console.log('color index:', ci);
     }
   }
 
   render (){
-    const { quotes, index } = this.state;
+    const { quotes, index, color } = this.state;
     let quote = quotes[index];
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />    
             FCC: Random Quote Machine
           </p>
         </header>
 
-        <div id="quote-box" className="wrapper bg-dark">
+        <div id="quote-box" className="wrapper"
+        style={{ background: color }}>
           <div className="col-6 box p-5 rounded">
               {
                 quote && (
                   <div id="text" className="mb-4">
                     <cite><p>{quote.quote}</p></cite>
-                    <p id="author" className="d-block text-right">-{quote.author}</p>
+                    <p id="author" className="d-block text-right">—{quote.author}</p>
                   </div>
                 )
               }
             <div className="d-flex justify-content-between">
               <a id="tweet-quote" className="btn btn-primary"
-              href={quote&&`${tweetURL}"${quote.quote}"--${quote.author} `} 
+              href={quote&&`${tweetURL}"${quote.quote}"—${quote.author} `} 
               target="_blank" rel="noopener noreferrer">
               <i className="fab fa-twitter"></i> Tweet</a>
               <button id="new-quote" className="btn btn-primary" onClick={this.RandomizeIndex}>New Quote</button>
